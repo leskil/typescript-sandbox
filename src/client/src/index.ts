b43c90ff874e84b6068cc0f973433970ef4e8dfa
Lasse Eskildsen <les@vertica.dk>
2017-11-08 17:11:04 +0100

    Initial stuff

---
+++ src/client/src/index.ts
@@ -0,0 +1,5 @@
+export class HelloWorld {
+    SayHello(message: string): number {
+        return -1;
+    }
+}
---
+++ src/client/src/webpack.config.js
@@ -0,0 +1,33 @@
+var Path = require("path");
+
+module.exports = {
+    entry: "./index.ts",
+    output: {
+        filename: "bundle.js",
+        path: Path.resolve(__dirname, "../assets")
+    },
+    devtool: "source-map",
+    watch: true,
+    devServer: {
+        contentBase: "./.."
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        modules: [
+            Path.join(__dirname, 'node_modules')
+            //"./node_modules/**"
+            //Path.resolve("./node_modules")
+        ]
+        //modulesDirectories: ["./node_modules"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader"
+            }
+        ]
+    },
+    plugins: [
+    ]
+};
