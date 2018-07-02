b18770d324bcde5177726967a9ce9c4e9512ac12
Lasse Eskildsen <les@vertica.dk>
2018-07-02 22:49:07 +0200

    WIP: new computer

---
+++ src/client/src/sorting/merge-sort.tests.ts
@@ -0,0 +1,19 @@
+import { assert } from "chai";
+import { MergeSort } from "./merge-sort";
+
+describe("Merge sort", () => {
+    
+        it("Should sort a list of unique numbers", () => {
+            let input = [5, 6, 3, 1, 2, 7, 4];
+            let output = MergeSort.sort(input);
+            assert.includeOrderedMembers(output, [1, 2, 3, 4, 5, 6, 7])
+        });
+    
+        it("Should sort a list of non-unique numbers", () => {
+            let input = [5, 6, 3, 1, 2, 2, 7, 4, 4];
+            let output = MergeSort.sort(input);
+    
+            assert.includeOrderedMembers(output, [1, 2, 2, 3, 4, 4, 5, 6, 7]);
+        });
+    
+    });
---
+++ src/client/src/sorting/merge-sort.ts
@@ -0,0 +1,11 @@
+export class MergeSort {
+
+    static sort<T>(arr: Array<T>): Array<T> {
+
+        return arr;
+        // 1. Del array i to
+        // 2. Hvis 
+
+    }
+
+}
